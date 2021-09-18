799ea7ddc410761f40744f69a4a8afa682310a6e
Fernan344 <41765781+Fernan344@users.noreply.github.com>
2021-09-17 18:15:53 -0600

    Add files via upload

--- Node_Client/src/Controller/client.controller.js
+++ Node_Client/src/Controller/client.controller.js
@@ -6,39 +6,114 @@ exports.test = async (req, res) => {
 }
 
 exports.getTweets = async (req, res) => {       
-    await pool.query('SELECT * FROM twit', async function(error, result, fields){
-        let results = [];    
-        results = result.map(async (data) => {              
-            await pool.query('SELECT h.hashtag FROM hashtags h INNER JOIN hash_twit ht ON ht.idHash = h.id WHERE ht.idTwit = ?', 
-                                                    [data["id"]], async function(error, result2, fields){
-                let results = [];   
-                results = result2.map(data => {
-                    return data["hashtag"]
-                })
-
-                let array = {
-                    "id": data["id"],
-                    "nombre": data["nombre"],
-                    "comentario": data["comentario"],
-                    "fecha": data["fecha"],
-                    "upvotes": data["upvotes"],
-                    "downvotes": data["downvotes"],
-                    "hashtags": JSON.stringify(results)
-                }
-
-                return array
-            }); 
-            console.log(resultadoHash)
-        })   
+    let result = await pool.query('SELECT t.id, t.nombre, t.comentario, t.fecha, t.upvotes, t.downvotes, REPLACE(GROUP_CONCAT(distinct h.hashtag), \',\', \' #\') hashtags FROM hash_twit ht INNER JOIN twit t ON t.id=ht.idTwit INNER JOIN hashtags h ON h.id=ht.idHash GROUP BY t.id ORDER BY t.fecha DESC');
         
-        if (results.length != 0){ 
-            await res.json({                
-                "null":false,
-                "data": JSON.stringify(results)
-            });
-        }else{
-            await res.json({"null":true});
-        }     
+    let results = [];
+    results = result.map((data) => {      
+
+        let array = {
+            "id": data["id"],
+            "nombre": data["nombre"],
+            "comentario": data["comentario"],
+            "fecha": data["fecha"],
+            "upvotes": data["upvotes"],
+            "downvotes": data["downvotes"],
+            "hashtags": "#"+data["hashtags"]
+        }
+        return array
+    })   
+
+    if (results.length != 0){ 
+        await res.json({                
+            "null":false,
+            "data": JSON.stringify(results)
+        });
+    }else{
+        await res.json({"null":true});
+    }  
+    
+    /*console.log(req.body)
+    let socket = theSocket.getSocket();
+    socket.emit('addTweet', req.body);
+    res.json({"saludos": "jsjsjs"})*/
+}
+
+exports.reportesMySQL = async (req, res) => {   
+    let reporte = {
+        "twits": 0,
+        "hashtags": 0,
+        "upvotes": 0,
+        "tophash": [],
+        "tophashData": [],
+        "days": [],
+        "upvotesByDay": [],
+        "downvotesByDay": [],
+        "last100entrys": []
+    }
+
+    let noticias = await pool.query('SELECT COUNT(id) total FROM twit');
+     
+    reporte.twits = noticias[0]["total"]
+
+    let hashtags = await pool.query('SELECT COUNT(id) total FROM hashtags');
+
+    reporte.hashtags = hashtags[0]["total"]
+
+    let upvotes = await pool.query('SELECT SUM(upvotes) total FROM twit');
+
+    reporte.upvotes = upvotes[0]["total"]
+
+    let top5Hash = await pool.query('SELECT SUM(t.upvotes) total, h.hashtag FROM hash_twit ht INNER JOIN twit t ON t.id=ht.idTwit INNER JOIN hashtags h ON h.id=ht.idHash GROUP BY idHash ORDER BY total DESC LIMIT 5');
+        
+    let resultsTopH = [];
+    let dataTopH = [];
+
+    top5Hash.map((data) => {     
+        resultsTopH.push(data["hashtag"])
+        dataTopH.push(data["total"])
+    })   
+
+    reporte.tophash = (resultsTopH)
+    reporte.tophashData = (dataTopH)
+
+    let lastEntries = await pool.query('SELECT t.id, t.nombre, t.comentario, t.fecha, t.upvotes, t.downvotes, REPLACE(GROUP_CONCAT(distinct h.hashtag), \',\', \' #\') hashtags FROM hash_twit ht INNER JOIN twit t ON t.id=ht.idTwit INNER JOIN hashtags h ON h.id=ht.idHash GROUP BY t.id ORDER BY t.fecha DESC LIMIT 100');
+        
+    let resultsEntries = [];
+    resultsEntries = lastEntries.map((data) => { 
+        
+        let array = [
+            data["nombre"],
+            data["fecha"],
+            data["comentario"],            
+            data["upvotes"],
+            data["downvotes"],
+            "#"+data["hashtags"]
+        ]
+        return array
+    }) 
+
+    reporte.last100entrys = (resultsEntries)
+
+    let votesByDate = await pool.query('SELECT SUM(upvotes) upvotes, SUM(downvotes) downvotes, fecha FROM twit WHERE fecha BETWEEN \'2015-02-23\' AND \'2015-02-28\' GROUP BY fecha');
+        
+    let labelsVotes = [];
+    let numUpvotes = [];
+    let numDownVotes = [];
+
+    votesByDate.map((data) => { 
+        
+        labelsVotes.push(data["fecha"])
+        numUpvotes.push(data["upvotes"])
+        numDownVotes.push(data["downvotes"]*-1)
+    }) 
+
+    reporte.days = labelsVotes
+    reporte.upvotesByDay = numUpvotes
+    reporte.downvotesByDay = numDownVotes
+
+    res.json({                
+        "null":false,
+        "data": reporte
     });
     
     /*console.log(req.body)
--- Node_Client/src/Routes/client.router.js
+++ Node_Client/src/Routes/client.router.js
@@ -4,5 +4,6 @@ const clientController = require("../Controller/client.controller")
 
 router.get("/", clientController.test)
 router.get("/getTweets", clientController.getTweets)
+router.post("/getReportsMySQL", clientController.reportesMySQL)
 
 module.exports = router
