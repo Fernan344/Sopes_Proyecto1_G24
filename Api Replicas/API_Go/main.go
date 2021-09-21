a81697085be6b9b93b83ef879afa0da18dfc7b6b
Fernan344 <41765781+Fernan344@users.noreply.github.com>
2021-09-21 01:04:23 -0600

    Add files via upload

--- Api Replicas/API_Go/main.go
+++ Api Replicas/API_Go/main.go
@@ -1,117 +1,153 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"strconv"
+	"strings"
 
+	//"strconv"
+
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
-type task struct {
-	ID      int    `json:ID`
-	Name    string `json:Name`
-	Content string `json:Content`
+type twit struct {
+	Nombre     string   `json:nombre`
+	Comentario string   `json:comentario`
+	Fecha      string   `json:fecha`
+	Hashtags   []string `json:hashtags`
+	Upvotes    int      `json:upvotes`
+	Downvotes  int      `json:downvotes`
 }
 
-type allTasks []task
+func conexionDB() (conexion *sql.DB) {
+	Driver := "mysql"
+	Usuario := "root"
+	Contrasenia := "josePJ64"
+	Nombre := "twitter"
 
-var tasks = allTasks{
-	{
-		ID:      1,
-		Name:    "Task One",
-		Content: "Some Content",
-	},
-}
-
-func getTasks(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(tasks)
-}
-
-func getTask(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	taskId, err := strconv.Atoi(vars["id"])
+	conexion, err := sql.Open(Driver, Usuario+":"+Contrasenia+"@tcp(127.0.0.1)/"+Nombre)
 
 	if err != nil {
-		fmt.Fprintf(w, "Invalid ID")
-		return
+		panic(err.Error())
 	}
 
-	for _, task := range tasks {
-		if task.ID == taskId {
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(task)
-		}
-	}
-}
-
-func deleteTask(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	taskId, err := strconv.Atoi(vars["id"])
-
-	if err != nil {
-		fmt.Fprintf(w, "Invalid ID")
-		return
-	}
-
-	for index, task := range tasks {
-		if task.ID == taskId {
-			tasks = append(tasks[:index], tasks[index+1:]...)
-			fmt.Fprintf(w, "The task with ID %v has been remove succesfully", taskId)
-		}
-	}
-}
-
-func updateTask(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	taskId, err := strconv.Atoi(vars["id"])
-
-	var updatedTask task
-
-	if err != nil {
-		fmt.Fprintf(w, "Invalid ID")
-		return
-	}
-
-	reqBody, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Fprintf(w, "Insert a Valid Task")
-	}
-
-	json.Unmarshal(reqBody, &updatedTask)
-
-	for index, task := range tasks {
-		if task.ID == taskId {
-			tasks = append(tasks[:index], tasks[index+1:]...)
-			updatedTask.ID = taskId
-			tasks = append(tasks, updatedTask)
-			fmt.Fprintf(w, "The task with ID %v has been updated succesfully", taskId)
-		}
-	}
-}
-
-func createTask(w http.ResponseWriter, r *http.Request) {
-	var newTask task
-	reqBody, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Fprintf(w, "Insert a Valid Task")
-	}
-
-	json.Unmarshal(reqBody, &newTask)
-	newTask.ID = len(tasks) + 1
-	tasks = append(tasks, newTask)
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(newTask)
+	return conexion
 }
 
 func indexRoute(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome")
+}
+
+func publicar(w http.ResponseWriter, r *http.Request) {
+	var newTwit twit
+	reqBody, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		fmt.Fprintf(w, "Insert a Valid Twit")
+	}
+
+	json.Unmarshal(reqBody, &newTwit)
+
+	fechaParts := strings.Split(newTwit.Fecha, "/")
+	newFecha := fechaParts[2] + "-" + fechaParts[1] + "-" + fechaParts[0]
+	newTwit.Fecha = newFecha
+
+	fmt.Println(newTwit.Hashtags)
+
+	conexion := conexionDB()
+	insertarRegistros, err := conexion.Prepare("INSERT INTO TWIT(nombre, comentario, fecha, upvotes, downvotes, api) VALUES(?,?,?,?,?,'go')")
+	if err != nil {
+		panic(err)
+	}
+
+	resultado, err := insertarRegistros.Exec(newTwit.Nombre, newTwit.Comentario, newTwit.Fecha, newTwit.Upvotes, newTwit.Downvotes)
+
+	if err != nil {
+		fmt.Println(resultado)
+		panic(err)
+	}
+
+	registro, err := conexion.Query("SELECT MAX(id) FROM twit")
+
+	if err != nil {
+		panic(err)
+	}
+
+	var idTwit int
+
+	for registro.Next() {
+		err = registro.Scan(&idTwit)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	for i := 0; i < len(newTwit.Hashtags); i++ {
+		hashtag := newTwit.Hashtags[i]
+		insertarHashtag, err := conexion.Prepare("INSERT INTO hashtags (hashtag) VALUES (?)")
+
+		if err != nil {
+			registroIdHashtag, err := conexion.Query("SELECT id FROM hashtags WHERE hashtag = ?", hashtag)
+
+			if err != nil {
+				panic(err)
+			}
+
+			var idHash int
+			for registroIdHashtag.Next() {
+				err = registroIdHashtag.Scan(&idHash)
+				if err != nil {
+					panic(err)
+				}
+			}
+
+			insertarTwitHash, err := conexion.Prepare("INSERT INTO hash_twit (idTwit, idHash) VALUES (?, ?)")
+			if err != nil {
+				panic(err)
+			}
+			resultado, err := insertarTwitHash.Exec(idTwit, idHash)
+			if err != nil {
+				fmt.Println(resultado)
+				panic(err)
+			}
+		}
+
+		insertarHashtag.Exec(hashtag)
+
+		registroIdHashtag, err := conexion.Query("SELECT MAX(id) FROM hashtags")
+
+		if err != nil {
+			panic(err)
+		}
+
+		var idHash int
+		for registroIdHashtag.Next() {
+			err = registroIdHashtag.Scan(&idHash)
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		insertarTwitHash, err := conexion.Prepare("INSERT INTO hash_twit (idTwit, idHash) VALUES (?, ?)")
+		if err != nil {
+			panic(err)
+		}
+		resultado, err := insertarTwitHash.Exec(idTwit, idHash)
+
+		if err != nil {
+			fmt.Println(resultado)
+			panic(err)
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newTwit)
 }
 
 func main() {
@@ -120,10 +156,6 @@ func main() {
 
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", indexRoute)
-	router.HandleFunc("/tasks", getTasks).Methods("GET")
-	router.HandleFunc("/createTask", createTask).Methods("POST")
-	router.HandleFunc("/tasks/{id}", getTask).Methods("GET")
-	router.HandleFunc("/tasks/{id}", deleteTask).Methods("DELETE")
-	router.HandleFunc("/tasks/{id}", updateTask).Methods("PUT")
+	router.HandleFunc("/publicar", publicar).Methods("POST")
 	log.Fatal(http.ListenAndServe(":5000", router))
 }
