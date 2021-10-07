8bd2722c0b95836444c2dfb0977a675522030a3d
Fernan344 <41765781+Fernan344@users.noreply.github.com>
2021-10-07 07:21:44 -0600

    Add files via upload

--- Node_Client/src/index.js
+++ Node_Client/src/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const Server = require('socket.io')
 const WebSocketServer = require('socket.io')
 const http = require('http')
-const theSocket = require('./Recursos/Socket');
 const clientRouter = require('./Routes/client.router')
+const socketClass = require('./Controller/socket.controller');
+
+require('./Mongo/database')
 
 const morgan = require('morgan');
 const cors = require('cors')
@@ -15,7 +17,7 @@ app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 app.use(cors());
 app.use(morgan('dev'));
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 3001);
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
@@ -26,10 +28,10 @@ const httpServer = http.createServer(app);
 const io = WebSocketServer(httpServer)
 
 io.on('connection', (socket) => {
-    console.log('nueva Conexion', socket.id)
+    console.log("conectado: "+socket.id)
+    socketClass.sockets(socket)    
 })
 
 httpServer.listen(app.get('port'), ()=>{
-    console.log('server chambeando', app.get('port')); 
-    theSocket.setSocket(io);
+    console.log('server chambeando', app.get('port'));    
 });
